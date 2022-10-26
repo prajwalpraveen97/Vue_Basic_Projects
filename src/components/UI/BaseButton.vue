b6f1e51f1fdd2e3116fde34eecf7d88eb4a59e1a
Prajwal <prajwalpraveen97@gmail.com>
2022-10-26 17:40:45 +0200

    First commit

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,40 @@
+// Setting the css class based on the mode
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode'] // Using mode prop to set a CSS class
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
